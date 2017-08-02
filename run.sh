7442af6406dca4039ac2fd82885f89da790d91fa
semux <admin@semux.org>
2017-08-01 22:08:52 -0400

    Enhance Java runtime check

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@
 cd "$(dirname "$0")"
 
 # check java version
+if ! [ -x "$(command -v java)" ]; then
+    echo "Error: Java is not installed"
+    exit 1
+fi
 version=$(java -version 2>&1 | head -n 1 | cut -d'"' -f2 | cut -d'.' -f2)
 if [ $version -lt "8" ]; then
     echo "Error: Java 8 or above is required"
