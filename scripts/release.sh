fb6bc22883997e44a3005b1d52029d68d80103e3
semux <admin@semux.org>
2017-09-09 23:13:21 +0200

    Remove accidently added contents

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,22 +15,3 @@ zip -r $name.zip $name || exit
 
 # clean
 rm -fr $name
-
-/**
- * Get block reward for the given block.
- *
- * @param number
- *            block number
- * @return the block reward
- */
-public static long getBlockReward(long number) {
-    if (number <= 1_000_000) {
-        return 100 * Unit.SEM;
-    } else if (number <= 3_000_000) {
-        return 50 * Unit.SEM;
-    } else if (number <= 7_000_000) {
-        return 25 * Unit.SEM;
-    } else {
-        return 0;
-    }
-}
