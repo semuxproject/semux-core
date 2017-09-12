6442fb77f703bdbd13a8cb4c9cc970e81d9abb13
semux <admin@semux.org>
2017-09-11 21:52:59 -0400

    Move version check to gui

--- src/main/java/org/semux/GUI.java
+++ src/main/java/org/semux/GUI.java
@@ -8,9 +8,11 @@ package org.semux;
 
 import java.awt.EventQueue;
 import java.io.File;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -27,11 +29,22 @@ import org.semux.gui.Model;
 import org.semux.gui.WelcomeFrame;
 import org.semux.gui.dialog.InputDialog;
 import org.semux.gui.dialog.SelectDialog;
+import org.semux.utils.SystemUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xbill.DNS.Lookup;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.SimpleResolver;
+import org.xbill.DNS.TXTRecord;
+import org.xbill.DNS.TextParseException;
+import org.xbill.DNS.Type;
 
 /**
  * Graphic user interface.
  */
 public class GUI {
+
+    private static Logger logger = LoggerFactory.getLogger(GUI.class);
 
     private static final int TRANSACTION_LIMIT = 1000; // per account
 
@@ -121,6 +134,33 @@ public class GUI {
             MainFrame frame = new MainFrame(model);
             frame.setVisible(true);
         });
+
+        // start version check
+        new Thread(() -> {
+            try {
+                String domain = "version.semux.org";
+                Lookup lookup = new Lookup(domain, Type.TXT);
+                lookup.setResolver(new SimpleResolver());
+                lookup.setCache(null);
+                Record[] records = lookup.run();
+
+                if (lookup.getResult() == Lookup.SUCCESSFUL) {
+                    for (Record record : records) {
+                        TXTRecord txt = (TXTRecord) record;
+                        for (Object str : txt.getStrings()) {
+                            String version = str.toString();
+
+                            if (SystemUtil.versionCompare(Config.CLIENT_VERSION, version) < 0) {
+                                JOptionPane.showMessageDialog(null, "Your wallet need to be upgraded!");
+                                System.exit(-1);
+                            }
+                        }
+                    }
+                }
+            } catch (TextParseException | UnknownHostException e) {
+                logger.debug("Failed to get min client version");
+            }
+        }).start();
     }
 
     private static void onBlockAdded(Block block) {
