e53057b183784107219314c98083622790a20441
semux <admin@semux.org>
2017-09-21 00:22:06 -0400

    Add diagnose tools

---
+++ src/main/java/org/semux/diagnose/AccountViewer.java
@@ -0,0 +1,9 @@
+package org.semux.diagnose;
+
+public class AccountViewer {
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/org/semux/diagnose/DatabaseViewer.java
+++ src/main/java/org/semux/diagnose/DatabaseViewer.java
@@ -8,14 +8,14 @@ import org.semux.db.KVDB;
 import org.semux.db.LevelDB;
 import org.semux.utils.ClosableIterator;
 
-public class DatabaseInspect {
+public class DatabaseViewer {
 
     public static void inspectDB(KVDB db) {
         ClosableIterator<Entry<byte[], byte[]>> itr = db.iterator();
         try {
             while (itr.hasNext()) {
                 Entry<byte[], byte[]> e = itr.next();
-                System.out.println(String.format("%-64s = %s", Hex.encode(e.getKey()), Hex.encode(e.getValue())));
+                System.out.println(Hex.encode(e.getKey()) + "\t: " + Hex.encode(e.getValue()));
             }
         } finally {
             if (itr != null) {
@@ -25,8 +25,9 @@ public class DatabaseInspect {
     }
 
     public static void inspectAll() {
+        System.out.println("Databases:");
         for (DBName name : DBName.values()) {
-            System.out.println("================ " + name + " ================");
+            System.out.println("\n================\n" + name + "\n================");
             KVDB kvdb = new LevelDB(name);
             inspectDB(kvdb);
         }
