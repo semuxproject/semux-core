16631cb9ffdc46ab14aeba7529d5677f086c574f
semux <admin@semux.org>
2017-10-05 20:10:14 -0400

    Bump version

--- src/main/java/org/semux/Config.java
+++ src/main/java/org/semux/Config.java
@@ -155,7 +155,7 @@ public class Config {
     /**
      * Version of this client.
      */
-    public static String CLIENT_VERSION = "1.0.0-beta";
+    public static String CLIENT_VERSION = "1.0.0-beta.1";
 
     // =========================
     // Crypto
