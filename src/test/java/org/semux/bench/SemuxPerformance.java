7e9e86886f41433016d548c3f6168e6275e4c815
semux <admin@semux.org>
2017-10-29 15:33:05 -0400

    Update Semux performance test

--- src/test/java/org/semux/bench/SemuxPerformance.java
+++ src/test/java/org/semux/bench/SemuxPerformance.java
@@ -1,6 +1,5 @@
 package org.semux.bench;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.text.SimpleDateFormat;
@@ -11,30 +10,23 @@ import java.util.Map;
 import org.json.JSONObject;
 import org.semux.Config;
 import org.semux.core.Unit;
-import org.semux.core.Wallet;
-import org.semux.crypto.EdDSA;
 import org.semux.utils.ApiUtil;
 import org.semux.utils.Bytes;
 import org.semux.utils.SystemUtil;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SemuxPerformance {
-    private static Logger logger = LoggerFactory.getLogger(SemuxPerformance.class);
-
-    private static Wallet wallet = new Wallet(new File("wallet.data"));
     private static String password;
 
-    private static InetSocketAddress server = new InetSocketAddress("127.0.0.1", 5171);
-    private static int coinbase = 0;
+    private static InetSocketAddress server = new InetSocketAddress("localhost", 5171);
+    private static int coinbase = 1;
     private static int tps = 500;
 
-    public static void testTransfer(EdDSA key, int n) throws IOException, InterruptedException {
+    public static void testTransfer(int n) throws IOException, InterruptedException {
         long t1 = System.currentTimeMillis();
         for (int i = 1; i <= n; i++) {
             Map<String, Object> params = new HashMap<>();
-            params.put("from", key.toAddress());
-            params.put("to", key.toAddress());
+            params.put("from", coinbase);
+            params.put("to", coinbase);
             params.put("value", 1 * Unit.MILLI_SEM);
             params.put("fee", Config.MIN_TRANSACTION_FEE_SOFT);
             params.put("data", Bytes.EMPY_BYTES);
@@ -58,28 +50,13 @@ public class SemuxPerformance {
     public static void main(String[] args) throws Exception {
         password = SystemUtil.readPassword();
 
-        if (!wallet.unlock(password)) {
-            logger.error("Failed to unlock your wallet");
-            return;
-        }
-
-        if (wallet.getAccounts().isEmpty()) {
-            logger.error("No accounts in your wallet");
-            return;
-        }
-
-        EdDSA key = wallet.getAccounts().get(coinbase);
-        JSONObject obj = ApiUtil.request(server, "get_account", "address", key.toAddressString());
-        System.out.println("Address: " + key.toAddressString());
-        System.out.println("Balance: " + obj.getJSONObject("result").getLong("balance") / Unit.SEM + " SEM");
-
         while (true) {
             System.out.print("# transactions to send: ");
             System.out.flush();
 
             int n = Integer.parseInt(SystemUtil.SCANNER.nextLine().replaceAll("[^\\d]", ""));
             if (n > 0) {
-                testTransfer(key, n);
+                testTransfer(n);
             } else {
                 break;
             }
