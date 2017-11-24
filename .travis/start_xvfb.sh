2fc299d678564846a1ec610064b835793908f0b3
cryptokat <32390172+cryptokat@users.noreply.github.com>
2017-11-24 16:14:36 +0800

    Tools: start xvfb in travis-ci for GUI tests

---
+++ .travis/start_xvfb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DISPLAY=:99.0
+/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
