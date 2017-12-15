29fbcf860b8a597e3b2f0d3789b1245d07a5eba3
cryptokat <32390172+cryptokat@users.noreply.github.com>
2017-12-15 21:38:09 +0800

    Tests: improve transfer many integration test

--- src/test/java/org/semux/integration/TransferManyTransactionIntegrationTest.java
+++ src/test/java/org/semux/integration/TransferManyTransactionIntegrationTest.java
@@ -7,6 +7,7 @@
 package org.semux.integration;
 
 import static org.awaitility.Awaitility.await;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
@@ -27,7 +28,6 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -42,8 +42,10 @@ import org.semux.config.Config;
 import org.semux.config.Constants;
 import org.semux.config.DevNetConfig;
 import org.semux.core.Genesis;
+import org.semux.core.Transaction;
 import org.semux.core.Unit;
 import org.semux.core.Wallet;
+import org.semux.core.state.Account;
 import org.semux.crypto.EdDSA;
 import org.semux.net.NodeManager;
 import org.semux.util.ApiClient;
@@ -60,6 +62,8 @@ public class TransferManyTransactionIntegrationTest {
 
     private static Logger logger = LoggerFactory.getLogger(TransferManyTransactionIntegrationTest.class);
 
+    private static final long PREMINE = 1000000;
+
     @Rule
     public TemporaryFolder kernel1Folder = new TemporaryFolder();
 
@@ -69,68 +73,102 @@ public class TransferManyTransactionIntegrationTest {
     @Rule
     public TemporaryFolder kernel3Folder = new TemporaryFolder();
 
-    private List<KernelMock> kernels;
+    @Rule
+    public TemporaryFolder kernel4Folder = new TemporaryFolder();
+
+    public KernelMock kernelValidator;
+
+    public KernelMock kernelPremine;
+
+    public KernelMock kernelReceiver1;
+
+    public KernelMock kernelReceiver2;
 
     @Before
     public void setUp() throws IOException {
-        kernels = new ArrayList<>();
-        kernels.add(mockKernel(51610, 51710, kernel1Folder));
-        kernels.add(mockKernel(51620, 51720, kernel2Folder));
-        kernels.add(mockKernel(51630, 51730, kernel3Folder));
+        kernelValidator = mockKernel(51610, 51710, kernel1Folder);
+        kernelPremine = mockKernel(51620, 51720, kernel2Folder);
+        kernelReceiver1 = mockKernel(51630, 51730, kernel3Folder);
+        kernelReceiver2 = mockKernel(51640, 51740, kernel4Folder);
 
         // mock genesis.json
-        Genesis genesis = mockGenesis(kernels.get(0).getWallet().getAccount(0).toAddressString());
+        Genesis genesis = mockGenesis();
         mockStatic(Genesis.class);
         when(Genesis.load(any())).thenReturn(genesis);
 
         // mock seed nodes
         Set<InetSocketAddress> nodes = new HashSet<>();
         nodes.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 51610));
+        nodes.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 51620));
+        nodes.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 51630));
+        nodes.add(new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 51640));
         mockStatic(NodeManager.class);
         when(NodeManager.getSeedNodes(Constants.DEV_NET_ID)).thenReturn(nodes);
     }
 
-    @After
-    public void tearDown() {
-    }
-
+    /**
+     * Expectations:
+     * 1. kernelReceiver1 and kernelReceiver2 should receive 1000 SEM from
+     * kernelPremine 2. kernelPremine's account should be deducted with 1000 SEM * 2
+     * + 0.05 SEM * 2
+     */
     @Test
     public void testTransferManyTransaction() throws IOException {
-        KernelMock kernel1 = kernels.get(0);
-        KernelMock kernel2 = kernels.get(1);
-        KernelMock kernel3 = kernels.get(2);
-
         // start kernels
-        new Thread(kernel1::start, "kernel1").start();
-        await().until(() -> kernel1.getApi() != null && kernel1.getApi().isRunning());
-        logger.info("Kernel-1 API Started");
+        new Thread(kernelValidator::start, "kernelValidator").start();
+        new Thread(kernelPremine::start, "kernelPremine").start();
+        new Thread(kernelReceiver1::start, "kernelReceiver1").start();
+        new Thread(kernelReceiver2::start, "kernelReceiver2").start();
+        await().until(() -> kernelValidator.getApi() != null && kernelValidator.getApi().isRunning());
+        await().until(() -> kernelPremine.getApi() != null && kernelPremine.getApi().isRunning());
+        await().until(() -> kernelReceiver1.getApi() != null && kernelReceiver1.getApi().isRunning());
+        await().until(() -> kernelReceiver2.getApi() != null && kernelReceiver2.getApi().isRunning());
 
-        new Thread(kernel2::start, "kernel2").start();
-        new Thread(kernel3::start, "kernel3").start();
-
-        // make transfer_many request
+        // make transfer_many request from kernelPremine to kernelReceiver1 and
+        // kernelReceiver2
         final long value = 1000 * Unit.SEM;
+        final long fee = kernelPremine.getConfig().minTransactionFee() * 2;
         ApiClient apiClient = new ApiClient(
-                new InetSocketAddress("127.0.0.1", 51710),
+                new InetSocketAddress("127.0.0.1", 51720),
                 "user",
                 "pass");
-
         HashMap<String, Object> params = new HashMap<>();
-        params.put("from", kernel1.getWallet().getAccount(0).toAddressString());
-        params.put("to", kernel2.getWallet().getAccount(0).toAddressString() + ","
-                + kernel3.getWallet().getAccount(0).toAddressString());
+        params.put("from", kernelPremine.getWallet().getAccount(0).toAddressString());
+        params.put("to", kernelReceiver1.getWallet().getAccount(0).toAddressString() + ","
+                + kernelReceiver2.getWallet().getAccount(0).toAddressString());
         params.put("value", String.valueOf(value));
-        params.put("fee", String.valueOf(kernel1.getConfig().minTransactionFee() * 2));
+        params.put("fee", String.valueOf(fee));
         String response = apiClient.request("transfer_many", params);
         Map<String, String> result = new ObjectMapper().readValue(response, new TypeReference<Map<String, String>>() {
         });
         assertEquals("true", result.get("success"));
 
-        // wait until both of kernel2 and kernel3 have received the transaction
-        await().atMost(3, TimeUnit.MINUTES).until(() -> kernel2.getBlockchain().getAccountState()
-                .getAccount(kernel2.getWallet().getAccounts().get(0).toAddress()).getAvailable() == value &&
-                kernel3.getBlockchain().getAccountState()
-                        .getAccount(kernel2.getWallet().getAccounts().get(0).toAddress()).getAvailable() == value);
+        // wait until both of kernelReceiver1 and kernelReceiver2 have received the
+        // transaction
+        await().atMost(3, TimeUnit.MINUTES).until(() -> kernelReceiver1.getBlockchain().getAccountState()
+                .getAccount(kernelReceiver1.getWallet().getAccounts().get(0).toAddress()).getAvailable() == value &&
+                kernelReceiver2.getBlockchain().getAccountState()
+                        .getAccount(kernelReceiver2.getWallet().getAccounts().get(0).toAddress())
+                        .getAvailable() == value);
+
+        // (2x transaction value + 2x min transaction fee) should be deducted from
+        // kernelPremine's account
+        Account accountPremine = kernelPremine.getBlockchain().getAccountState()
+                .getAccount(kernelPremine.getWallet().getAccount(0).toAddress());
+        Account accountReceiver1 = kernelReceiver1.getBlockchain().getAccountState()
+                .getAccount(kernelReceiver1.getWallet().getAccount(0).toAddress());
+        Account accountReceiver2 = kernelReceiver2.getBlockchain().getAccountState()
+                .getAccount(kernelReceiver2.getWallet().getAccount(0).toAddress());
+        assertEquals(0, kernelValidator.getPendingManager().getTransactions().size());
+        List<Transaction> transactions = kernelPremine.getBlockchain().getTransactions(
+                kernelPremine.getWallet().getAccount(0).toAddress(),
+                0,
+                1);
+        assertArrayEquals(accountPremine.getAddress(), transactions.get(0).getFrom());
+        assertArrayEquals(accountReceiver1.getAddress(), transactions.get(0).getRecipient(0));
+        assertArrayEquals(accountReceiver2.getAddress(), transactions.get(0).getRecipient(1));
+        assertEquals(value, transactions.get(0).getValue());
+        assertEquals(PREMINE * Unit.SEM - value * 2 - fee, accountPremine.getAvailable());
     }
 
     private KernelMock mockKernel(int p2pPort, int apiPort, TemporaryFolder folder) throws IOException {
@@ -157,18 +195,18 @@ public class TransferManyTransactionIntegrationTest {
         return kernelMock;
     }
 
-    private Genesis mockGenesis(String premineAddress) {
+    private Genesis mockGenesis() {
+        String premineAddress = kernelPremine.getWallet().getAccount(0).toAddressString();
+        String delegateAddress = kernelValidator.getWallet().getAccount(0).toAddressString();
+
         // mock premine
-        Genesis.Premine premine = new Genesis.Premine(
-                premineAddress,
-                1000000,
-                "premine");
+        Genesis.Premine premine = new Genesis.Premine(premineAddress, PREMINE, "premine");
         List<Genesis.Premine> premines = new ArrayList<>();
         premines.add(premine);
 
         // mock delegates
         HashMap<String, String> delegates = new HashMap<>();
-        delegates.put("delegate", premineAddress);
+        delegates.put("delegate", delegateAddress);
 
         return Genesis.jsonCreator(
                 0,
@@ -178,7 +216,7 @@ public class TransferManyTransactionIntegrationTest {
                 "semux",
                 premines,
                 delegates,
-                new HashMap<String, Object>());
+                new HashMap<>());
     }
 
     private Wallet mockWallet(TemporaryFolder folder) throws IOException {
