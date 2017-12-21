085d349b3ee4fa564e689f4f47346b36f7cc1df6
semux <admin@semux.org>
2017-12-21 00:18:26 -0500

    Tools: update release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 version=`cat pom.xml | grep '^    <version>.*</version>$' | awk -F'[><]' '{print $3}'`
-name=semux-${version}
+name=semux
 
 # change work directory
 cd "$(dirname "$0")/../"
@@ -9,14 +9,22 @@ cd "$(dirname "$0")/../"
 # build
 mvn clean install -DskipTests || exit
 
-# archive
+# go to dist
 cd dist
-mv unix ${name}-unix
-tar -czvf ${name}-unix.tar.gz ${name}-unix || exit
-mv macos ${name}-macos
-tar -czvf ${name}-macos.tar.gz ${name}-macos || exit
-mv windows ${name}-windows
-zip -r ${name}-windows.zip ${name}-windows || exit
+
+# Windows
+mv windows ${name}-windows-${version}
+zip -r ${name}-windows-${version}.zip ${name}-windows-${version} || exit
+
+# Linux
+mv linux ${name}-linux-${version}
+tar -czvf ${name}-linux-${version}.tar.gz ${name}-linux-${version} || exit
+
+# macOS
+mv macos ${name}-macos-${version}
+tar -czvf ${name}-macos-${version}.tar.gz ${name}-macos-${version} || exit
 
 # clean
-rm -r ${name}-unix ${name}-macos ${name}-windows
+rm -r ${name}-windows-${version}
+rm -r ${name}-linux-${version} 
+rm -r ${name}-macos-${version}
