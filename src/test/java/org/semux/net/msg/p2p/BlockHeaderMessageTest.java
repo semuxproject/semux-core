95f814c1fdc94325f306f1c32c66c522fd359ae8
semux <admin@semux.org>
2017-12-30 09:58:59 -0500

    Tests: add GetBlockHeaderMessage test

---
+++ src/test/java/org/semux/net/msg/p2p/BlockHeaderMessageTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2017 The Semux Developers
+ *
+ * Distributed under the MIT software license, see the accompanying file
+ * LICENSE or https://opensource.org/licenses/mit-license.php
+ */
+package org.semux.net.msg.p2p;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+
+import org.junit.Test;
+import org.semux.core.BlockHeader;
+import org.semux.crypto.EdDSA;
+import org.semux.net.msg.consensus.BlockHeaderMessage;
+import org.semux.util.Bytes;
+import org.semux.util.MerkleUtil;
+
+public class BlockHeaderMessageTest {
+
+    @Test
+    public void testSerialization() {
+        long number = 1;
+        byte[] coinbase = Bytes.random(EdDSA.ADDRESS_LEN);
+        byte[] prevHash = Bytes.random(32);
+        long timestamp = System.currentTimeMillis();
+        byte[] transactionsRoot = MerkleUtil.computeTransactionsRoot(Collections.emptyList());
+        byte[] resultsRoot = MerkleUtil.computeResultsRoot(Collections.emptyList());
+        byte[] stateRoot = Bytes.EMPTY_HASH;
+        byte[] data = {};
+
+        BlockHeader header = new BlockHeader(number, coinbase, prevHash, timestamp, transactionsRoot, resultsRoot,
+                stateRoot, data);
+
+        BlockHeaderMessage m = new BlockHeaderMessage(header);
+        BlockHeaderMessage m2 = new BlockHeaderMessage(m.getEncoded());
+
+        assertThat(m2.getHeader()).isEqualToComparingFieldByField(header);
+    }
+}
