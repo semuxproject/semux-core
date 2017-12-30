ef995596bb59bceb872be78a8506a1a07bc3c926
semux <admin@semux.org>
2017-12-30 09:44:36 -0500

    Tools: update message verifier

--- src/test/java/org/semux/message/Verifier.java
+++ src/test/java/org/semux/message/Verifier.java
@@ -21,18 +21,22 @@ public class Verifier {
     public static void main(String[] args) throws IOException {
         Properties props = new Properties();
         props.load(Verifier.class.getResourceAsStream("/org/semux/gui/messages.properties"));
+        props.load(Verifier.class.getResourceAsStream("/org/semux/cli/messages.properties"));
 
-        Collection<File> files = FileUtils.listFiles(new File("src/main/java/org/semux/gui"), new String[] { "java" },
+        Collection<File> files = FileUtils.listFiles(new File("src/main/java/org/semux"), new String[] { "java" },
                 true);
+        int n = 0;
         for (File file : files) {
             String content = FileUtils.readFileToString(file, Bytes.CHARSET);
-            Pattern p = Pattern.compile("GUIMessages.get\\(\"(.+?)\"");
+            Pattern p = Pattern.compile("Messages.get\\(\"(.+?)\"");
             Matcher m = p.matcher(content);
             while (m.find()) {
+                n++;
                 if (!props.containsKey(m.group(1))) {
                     System.err.println(m.group(1) + " is not in the messages.properties!");
                 }
             }
         }
+        System.out.println(n);
     }
 }
