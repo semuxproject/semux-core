2fbfbbbfa59dcbe60e7595eb21a9ff194ea95001
semux <admin@semux.org>
2017-12-30 18:05:20 -0500

    Trivial: plan for the integration test

---
+++ src/test/java/org/semux/integration/SyncTest.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2017 The Semux Developers
+ *
+ * Distributed under the MIT software license, see the accompanying file
+ * LICENSE or https://opensource.org/licenses/mit-license.php
+ */
+package org.semux.integration;
+
+import org.junit.experimental.categories.Category;
+import org.semux.IntegrationTest;
+
+@Category(IntegrationTest.class)
+public class SyncTest {
+
+}
