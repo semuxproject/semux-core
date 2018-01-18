fc1fe9d21d68c13ffeb813ea4ff469407e91e0d3
orogvany <orogvany@yahoo.com>
2018-01-17 21:07:36 -0800

    Trivial: fix imports

--- src/main/java/org/semux/gui/model/WalletModel.java
+++ src/main/java/org/semux/gui/model/WalletModel.java
@@ -6,7 +6,7 @@
  */
 package org.semux.gui.model;
 
-import java.awt.*;
+import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
