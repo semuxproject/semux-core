ee0b53dcb5452259e60e9cb98a33046308112d23
semux <admin@semux.org>
2018-03-31 00:44:15 -0400

    Crypto: add semux native crypto interface

---
+++ src/main/java/org/semux/crypto/Native.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2017-2018 The Semux Developers
+ *
+ * Distributed under the MIT software license, see the accompanying file
+ * LICENSE or https://opensource.org/licenses/mit-license.php
+ */
+package org.semux.crypto;
+
+/**
+ * Semux crypto native implementation
+ */
+public class Native {
+
+    /**
+     * Computes the blake2b hash.
+     *
+     * @param data
+     * @return
+     */
+    public native byte[] blake2b(byte[] data);
+
+    /**
+     * Creates an Ed25519 message signature with the given private key.
+     *
+     * @param msg
+     * @param privateKey
+     * @return
+     */
+    public native byte[] ed25519_sign(byte[] msg, byte[] privateKey);
+
+    /**
+     * Verifies an Ed25519 signature.
+     *
+     * @param signature
+     * @param publicKey
+     * @return
+     */
+    public native boolean ed25519_verify(byte[] signature, byte[] publicKey);
+}
