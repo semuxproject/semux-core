9ef100365fc6dfb1528a4a85ca033de1d2cac513
orogvany <orogvany@yahoo.com>
2018-06-16 11:37:52 -0700

    Trivial: remove unused import

--- src/main/java/org/semux/gui/panel/SendPanel.java
+++ src/main/java/org/semux/gui/panel/SendPanel.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.TreeSet;
 
 import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
