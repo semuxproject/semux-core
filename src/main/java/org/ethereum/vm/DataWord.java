25b3d8f65464ca531e39e1cbc0935a434ee1cf9c
semux <admin@semux.org>
2018-08-11 10:14:41 -0400

    VM: add message call and log info

--- src/main/java/org/ethereum/vm/DataWord.java
+++ src/main/java/org/ethereum/vm/DataWord.java
@@ -4,6 +4,23 @@
  * Distributed under the MIT software license, see the accompanying file
  * LICENSE or https://opensource.org/licenses/mit-license.php
  */
+/*
+ * Copyright (c) [2016] [ <ether.camp> ]
+ * This file is part of the ethereumJ library.
+ *
+ * The ethereumJ library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ethereumJ library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.ethereum.vm;
 
 import java.math.BigInteger;
@@ -43,10 +60,6 @@ public class DataWord implements Comparable<DataWord> {
 
     public DataWord(BigInteger num) {
         this(num.toByteArray());
-    }
-
-    private DataWord(ByteBuffer buffer) {
-        this(buffer.array());
     }
 
     public DataWord(String data) {
