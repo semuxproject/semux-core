bfc25a784392bf249057a96bf396dbc5bfb416b8
semux <admin@semux.org>
2018-08-26 18:37:15 -0400

    Trivial: remove eclipse warning

--- src/main/java/org/semux/gui/panel/SendPanel.java
+++ src/main/java/org/semux/gui/panel/SendPanel.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 import javax.swing.ButtonGroup;
