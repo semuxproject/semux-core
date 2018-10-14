660fcd076ca862b811edaf99a4462c7fcf74165d
semux <admin@semux.org>
2018-10-14 10:52:50 +0800

    Trivial: remove unused imports

--- src/main/java/org/semux/Kernel.java
+++ src/main/java/org/semux/Kernel.java
@@ -12,8 +12,6 @@ import java.net.InetAddress;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import javax.xml.parsers.ParserConfigurationException;
 
