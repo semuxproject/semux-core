c57a477cadf34e05216dabaeff3ea748b4ad9513
orogvany <orogvany@yahoo.com>
2018-11-07 17:22:39 -0800

    Trivial: formatting/license headers

--- src/main/java/org/semux/config/AbstractConfig.java
+++ src/main/java/org/semux/config/AbstractConfig.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2017-2018 The Semux Developers
- * <p>
+ *
  * Distributed under the MIT software license, see the accompanying file
  * LICENSE or https://opensource.org/licenses/mit-license.php
  */
