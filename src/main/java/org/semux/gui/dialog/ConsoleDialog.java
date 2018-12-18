da06f14f3d6d231e9d716b5045321d7f890705bd
orogvany <orogvany@yahoo.com>
2018-12-18 11:26:02 -0800

    Trivial: formatting
    This was done in other CR's but missed them.
    
    Will catch formatting when we merge develop into RC2 to see
     what's missing.

--- src/main/java/org/semux/gui/dialog/ConsoleDialog.java
+++ src/main/java/org/semux/gui/dialog/ConsoleDialog.java
@@ -78,7 +78,6 @@ public class ConsoleDialog extends JDialog implements ActionListener {
         input.addActionListener(this);
         input.setName("txtInput");
 
-
         // add history for cycling through past commands
         input.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(
                 "UP"), "historyBack");
@@ -86,7 +85,7 @@ public class ConsoleDialog extends JDialog implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String command = commandHistory.back();
-                if(command!=null) {
+                if (command != null) {
                     input.setText(command);
                 }
             }
@@ -97,7 +96,7 @@ public class ConsoleDialog extends JDialog implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String command = commandHistory.forward();
-                if(command!=null) {
+                if (command != null) {
                     input.setText(command);
                 }
             }
