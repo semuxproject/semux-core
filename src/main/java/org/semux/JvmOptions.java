8038179fa55e05ea8e264a666c335bde0e8b1ec0
orogvany <orogvany@yahoo.com>
2019-01-28 20:27:20 -0800

    CLI: specify encoding
    mostly to silence findbugs, as we know the encoding will not matter

--- src/main/java/org/semux/JvmOptions.java
+++ src/main/java/org/semux/JvmOptions.java
@@ -8,18 +8,20 @@ package org.semux;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import org.semux.util.SystemUtil;
 
 public class JvmOptions {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         // System.out is replaced with a byte buffer to prohibit any outputs from other
         // places.
         // This is a work-around for ISSUE-119
         PrintStream out = System.out;
-        System.setOut(new PrintStream(new ByteArrayOutputStream()));
+        System.setOut(new PrintStream(new ByteArrayOutputStream(), true, "UTF-8"));
 
         StringBuilder sb = new StringBuilder();
         sb.append(" -Xmx").append(SystemUtil.getAvailableMemorySize() * 75 / 100 / 1024 / 1024).append("m");
