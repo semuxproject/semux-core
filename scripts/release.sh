6e1a0bccc59b77bb940fa22ec408934d74b1273b
semux <admin@semux.org>
2019-07-18 16:19:29 +0100

    Tools: pack JVM run time

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,24 +19,21 @@ WINDIST=${name}-windows-${version}
 WINBALL=${WINDIST}.zip
 mv windows ${WINDIST}
 zip -r ${WINBALL} ${WINDIST} || exit
-sha256sum ${WINBALL} > ${WINDIST}.sha256
-sha256sum --check ${WINDIST}.sha256
 
 # Linux
 LINUXDIST=${name}-linux-${version}
 LINUXBALL=${name}-linux-${version}.tar.gz
 mv linux ${LINUXDIST}
 tar -czvf ${LINUXBALL} ${LINUXDIST} || exit
-sha256sum ${LINUXBALL} > ${LINUXDIST}.sha256
-sha256sum --check ${LINUXDIST}.sha256
 
 # macOS
 MACDIST=${name}-macos-${version}
 MACBALL=${name}-macos-${version}.tar.gz
 mv macos ${MACDIST}
 tar -czvf ${MACBALL} ${MACDIST} || exit
-sha256sum ${MACBALL} > ${MACDIST}.sha256
-sha256sum --check ${MACDIST}.sha256
+
+# Checksum
+sha256sum *.tar.gz *.zip > sha256.txt
 
 # clean
 rm -r ${name}-windows-${version}
