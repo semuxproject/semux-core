53493995cd70de6003102339d083b97b014d0e75
semux <admin@semux.org>
2019-08-21 16:06:17 +0100

    Fix the semux gui launch script for windows
    Close: #253

--- scripts/semux-gui.bat
+++ scripts/semux-gui.bat
@@ -5,4 +5,4 @@ set java_bin=.\jvm\bin\javaw
 %java_bin% -cp semux.jar org.semux.JvmOptions --gui > jvm_options.txt
 set /p jvm_options=<jvm_options.txt
 
-start "" "%java_bin% %jvm_options% -cp semux.jar org.semux.Main --gui %*
+start "" "%java_bin%" %jvm_options% -cp semux.jar org.semux.Main --gui %*
